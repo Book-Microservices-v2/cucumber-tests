b7f94bee2a3574873c5903d8a924e3fa2b031e3e
Moises Macero <mechero@users.noreply.github.com>
2020-09-09 06:59:46 +0200

    Initial commit - archetype code

---
+++ src/test/java/microservices/book/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package microservices.book;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {
+
+}
---
+++ src/test/java/microservices/book/StepDefinitions.java
@@ -0,0 +1,11 @@
+package microservices.book;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class StepDefinitions {
+
+}
